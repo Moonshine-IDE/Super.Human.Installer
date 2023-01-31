a5c2e8eccd0640b3f8b7940259002dccbad93a7f
Adrian Szuszkiewicz <adrianszuszkiewicz@gmail.com>
2023-01-31 15:43:18 +0100

    Create chocolatey package
    (References: #2)

---
+++ Templates/chocolatey/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocaction = Join-Path $toolsDir 'SuperHumanInstallerDev-Setup.exe'
+$packageName = 'SuperHumanInstallerDev'
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'EXE'
+  file         	= $fileLocaction
+  softwareName  = 'SuperHumanInstallerDev'
+
+  # MSI
+  silentArgs    = "/S" #NSIS
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
