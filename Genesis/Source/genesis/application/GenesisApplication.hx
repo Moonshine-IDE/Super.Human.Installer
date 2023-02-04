9c017be353041921e932a1be7f4cf5f1c40de1f6
Tamas Sopronyi <tommysoprano@gmail.com>
2023-02-04 10:29:52 +0100

    Update GenesisApplication.hx

--- Genesis/Source/genesis/application/GenesisApplication.hx
+++ Genesis/Source/genesis/application/GenesisApplication.hx
@@ -69,6 +69,10 @@ import prominic.sys.applications.bin.Shell;
 import prominic.sys.tools.SysTools;
 import sys.FileSystem;
 
+#if build_macros
+@:build( BuildMacro.createGitInfo() )
+#end
+
 abstract class GenesisApplication extends Application {
 
     static public final GENESIS_ADDRESS:String = "https://genesis.directory/";
