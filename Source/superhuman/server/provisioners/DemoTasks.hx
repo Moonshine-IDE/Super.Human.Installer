4379543bfd52189e65a21adcf5b1a6f6e8a69ed4
Tamas Sopronyi <tommysoprano@gmail.com>
2023-02-07 14:09:04 +0100

    #43 Regex fixes

--- Source/superhuman/server/provisioners/DemoTasks.hx
+++ Source/superhuman/server/provisioners/DemoTasks.hx
@@ -52,7 +52,7 @@ class DemoTasks extends AbstractProvisioner {
     static final _IP_ADDRESS_PATTERN:EReg = ~/(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/;
     static final _SAFE_ID_FILE:String = "safe.ids";
     static final _SAFE_ID_LOCATION:String = "safe-id-to-cross-certify";
-    static final _TASK_IDENTIFIER_PATTERN:EReg = ~/^(?:\s+)(?:- name: )(\S+)$/m;
+    static final _TASK_IDENTIFIER_PATTERN:EReg = ~/(?:\s+)(?:- name: )(\S+)/;
     static final _VERSION_PATTERN:EReg = ~/(\d{1,3}\.\d{1,3}\.\d{1,3})/;
     static final _WEB_ADDRESS_PATTERN:EReg = ~/^(?:https:\/\/)(.*)(?::\d{3})(?:\/.*)$/gm;
 
