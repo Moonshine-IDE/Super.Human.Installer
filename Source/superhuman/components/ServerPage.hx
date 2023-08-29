b612de661c5e86cf22dda1d24f54e2db30e9f88a
Piotr Zarzycki <piotrzarzycki21@gmail.com>
2023-08-29 18:44:57 +0200

    Make directory open working
    (reference #94)

--- Source/superhuman/components/ServerPage.hx
+++ Source/superhuman/components/ServerPage.hx
@@ -214,8 +214,8 @@ class ServerPage extends Page {
         _serverList.addEventListener( SuperHumanApplicationEvent.STOP_SERVER, _forwardEvent );
         _serverList.addEventListener( SuperHumanApplicationEvent.SUSPEND_SERVER, _forwardEvent );
         _serverList.addEventListener( SuperHumanApplicationEvent.SYNC_SERVER, _forwardEvent );
-        #if debug
         _serverList.addEventListener( SuperHumanApplicationEvent.OPEN_SERVER_DIRECTORY, _forwardEvent );
+        #if debug
         _serverList.addEventListener( SuperHumanApplicationEvent.RESET_SERVER, _forwardEvent );
         #end
         _serverList.visible = _serverList.includeInLayout = _servers.length != 0 && _vagrantInstalled && _virtualBoxInstalled;
