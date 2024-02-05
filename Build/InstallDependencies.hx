e705771255d0568279aa033380cc2bec1d6b9e08
Adrian Szuszkiewicz <adrianszuszkiewicz@gmail.com>
2024-02-05 20:36:23 +0100

    Try simpler dependency script
    (References: #109)

--- Build/InstallDependencies.hx
+++ Build/InstallDependencies.hx
@@ -1,59 +1,14 @@
-// Usage:
-// $: haxe --run InstallDependencies.hx --target=windows --haxelibpath=C:/MoonshineSDKs/Haxe/lib
-
 class InstallDependencies {
-    public static function main() {
-        var namedArgs = parseNamedArguments(Sys.args());
-
-        trace('Installing dependencies...');
-        trace('target: ${namedArgs["target"]}');
-        trace('haxelibpath: ${namedArgs["haxelibpath"]}');
-        installDependencies(namedArgs['target'], namedArgs['haxelibpath']);
-    }
-
-    static function parseNamedArguments(args:Array<String>):Map<String, String> {
-        var result = new Map<String, String>();
-
-        for (arg in args) {
-            if (StringTools.startsWith(arg, "--")) {
-                var parts = arg.split('=');
-                var key = parts[0].substr(2); // Remove the "--" prefix
-                var value = parts.length > 1 ? parts[1] : "";
-                result.set(key, value);
-            }
-        }
-
-        return result;
-    }
-
-    public static function installDependencies(target:String, haxelibPath:String) {        
-        Sys.command('haxelib --global update haxelib --quiet --never');
-
-        // Install hxcpp
-        Sys.command('haxelib git hxcpp https://github.com/HaxeFoundation/hxcpp.git --quiet --never');
-        var buildDir = Sys.getCwd();
-        Sys.setCwd('${haxelibPath}/hxcpp/git/tools/hxcpp');
-        Sys.command('haxe compile.hxml');
-        Sys.setCwd(buildDir);     
-        
-        // Clone lime
+    public static function main() {        
+        Sys.command('haxelib install hxcpp');
         Sys.command('git clone --recursive --depth 1 --branch develop https://github.com/openfl/lime');
         Sys.command('haxelib dev lime lime');
-
-        // Install lime dependencies
-        Sys.command('haxelib install format --quiet');
-        Sys.command('haxelib install hxp --quiet');
-        Sys.command('haxelib git lime-samples https://github.com/openfl/lime-samples --quiet');
-
-        // Rebuild lime
-        Sys.command('haxelib run lime rebuild ${target} -release -clean');
-
-        // Install and setup openfl
-        Sys.command('haxelib git openfl https://github.com/openfl/openfl.git develop --quiet --never');
-        Sys.command('haxelib run openfl setup --quiet --never');
-
-        // Install other dependencies
-        Sys.command('haxelib git feathersui https://github.com/feathersui/feathersui-openfl.git --quiet --never');
-        Sys.command('haxelib git champaign https://github.com/Moonshine-IDE/Champaign.git --quiet --never');
+        Sys.command('haxelib install format');
+        Sys.command('haxelib install hxp');
+        Sys.command('haxelib git lime-samples https://github.com/openfl/lime-samples');
+        Sys.command('haxelib git openfl https://github.com/openfl/openfl.git develop');
+        Sys.command('haxelib run openfl setup');
+        Sys.command('haxelib git feathersui https://github.com/feathersui/feathersui-openfl.git');
+        Sys.command('haxelib git champaign https://github.com/Moonshine-IDE/Champaign.git');
     }
 }
