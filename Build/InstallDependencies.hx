ba41ad43eef4077a2dd8b8dab40d0746344e192c
Adrian Szuszkiewicz <adrianszuszkiewicz@gmail.com>
2024-02-05 21:40:07 +0100

    Hxp last
    (References: #109)

--- Build/InstallDependencies.hx
+++ Build/InstallDependencies.hx
@@ -4,11 +4,11 @@ class InstallDependencies {
         Sys.command('git clone --recursive --depth 1 --branch develop https://github.com/openfl/lime');
         Sys.command('haxelib dev lime lime');
         Sys.command('haxelib install format');
-        // Sys.command('haxelib install hxp');
         Sys.command('haxelib git lime-samples https://github.com/openfl/lime-samples');
         Sys.command('haxelib git openfl https://github.com/openfl/openfl.git develop');
         Sys.command('haxelib run openfl setup');
         Sys.command('haxelib git feathersui https://github.com/feathersui/feathersui-openfl.git');
         Sys.command('haxelib git champaign https://github.com/Moonshine-IDE/Champaign.git');
+        Sys.command('haxelib install hxp');
     }
 }
