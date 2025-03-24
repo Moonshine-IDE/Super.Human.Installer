0d3078eecdfbca424439015c8434e6b2b2d5c7eb
MarkProminic <Mark.Gilbert@prominic.net>
2025-03-24 15:14:59 -0500

    Dynamic Provisioner - Saving Changes

--- Source/superhuman/components/DynamicAdvancedConfigPage.hx
+++ Source/superhuman/components/DynamicAdvancedConfigPage.hx
@@ -257,7 +257,9 @@ class DynamicAdvancedConfigPage extends Page {
                         } else if (Std.isOfType(value, Float) || Std.isOfType(value, Int)) {
                             prop = new champaign.core.primitives.Property<String>(Std.string(value));
                         } else if (Std.isOfType(value, Bool)) {
-                            prop = new champaign.core.primitives.Property<Bool>(value);
+                            // Need to convert Boolean to Null<Bool> for Property constructor
+                            var boolValue:Null<Bool> = value;
+                            prop = new champaign.core.primitives.Property<Bool>(boolValue);
                         }
                         
                         if (prop != null) {
